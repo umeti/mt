64994336a427edec3e3d5d9deac63c8f41897cc7
umeti <yumeeti@gmail.com>
2021-10-24 21:18:39 +0800

    数据库安装

--- cronjobs/weather.js
+++ cronjobs/weather.js
@@ -1,12 +1,22 @@
 const axios = require('axios')
 
-async function tick(ctx,log){
-  let res = await axios.get('http://t.weather.itboy.net/api/weather/city/101280101')
-  log.info(res.data)
+async function tick(ctx, log,db) {
+  let res;
+  try {
+    log.info('请求天气数据...')
+    res = await axios.get('http://t.weather.itboy.net/api/weather/city/101280101',{
+      timeout:3000
+    })
+    log.info('缓存天气数据')
+    db.insert({type:'weather',data:res.data})
+  } catch (err) {
+    log.error(err)
+    log.debug(res)
+  }
 }
 
-function stop(){
+function stop() {
 
 }
 
-module.exports = {tick,stop}
+module.exports = { tick, stop }
--- server.js
+++ server.js
@@ -2,53 +2,63 @@ const cron = require('cron')
 const Koa = require('koa');
 const bunyan = require('bunyan');
 const yaml = require('yaml')
+const Datastore = require('nedb')
 
 const fs = require('fs')
 
 const log = bunyan.createLogger({
   name: "mt",
+  streams: [{
+    path: __dirname+'/logs/server.log',
+  }]
 });
 
 log.info('server up')
 
-log.info({event: 'load',data:'config'})
-const config = yaml.parse(fs.readFileSync(__dirname+'/config.yml','utf-8'))
+log.info({ event: 'load', data: 'config' })
+const config = yaml.parse(fs.readFileSync(__dirname + '/config.yml', 'utf-8'))
 
 let crontab = config.crontab
 
 // 加载任务模块
 let cronModule = {}
-for(let _ of crontab){
-  if(_.off) continue;
-  let name =`cronjobs/${_.task}.js` 
-  log.info({event: 'load',module:name})
-    try{
+for (let _ of crontab) {
+  if (_.off) continue;
+  let name = `cronjobs/${_.task}.js`
+  log.info({ event: 'load', module: name })
+  try {
     let m = require(`${__dirname}/${name}`)
     cronModule[_.task] = m
-  }catch(err){
-    log.error(_,err)
+  } catch (err) {
+    log.error(_, err)
   }
 }
 
+log.info({event:'load',data:'database'})
+const db = new Datastore({
+  filename: __dirname + '/data/data.db',
+  autoload: true
+});
+
 let cronjobs = []
-for(let _ of crontab){
+for (let _ of crontab) {
   let m = cronModule[_.task]
-  if(!m || _.off) continue;
+  if (!m || _.off) continue;
 
   let job = new cron.CronJob(
     _.rule,
-    async function(){
+    async function () {
       let cronLog = log.child({
-        event:'cronjob',
-        task:_.task,
+        event: 'cronjob',
+        task: _.task,
         id: this.lastDate()
       })
-      cronLog.info({action:'start'})
-      try{
-        await m.tick(this,cronLog)
-        cronLog.info({action:'done'})
-      }catch(err){
-        cronLog.error({action:'throw'},err)
+      cronLog.info({ action: 'start' })
+      try {
+        await m.tick(this, cronLog,db)
+        cronLog.info({ action: 'done' })
+      } catch (err) {
+        cronLog.error({ action: 'throw' }, err)
       }
     },
     m.stop, // TODO: 封装
@@ -59,20 +69,11 @@ for(let _ of crontab){
   cronjobs.push(job)
 }
 
-
-/*
-var CronJob = require('cron').CronJob;
-var job = new CronJob('* * * * * *', function() {
-    console.log('You will see this message every second');
-}, null, true, 'America/Los_Angeles');
-job.start();
-*/
-
-
-
+log.info('ready web server')
 const koapp = new Koa();
 koapp.use(async ctx => {
   ctx.body = 'Hello World';
 });
 
 koapp.listen(3000);
+log.info('web server up')
